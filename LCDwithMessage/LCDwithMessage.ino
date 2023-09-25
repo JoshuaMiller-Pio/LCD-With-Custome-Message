3d7eebc5bc9cf12f268c5fbf1e17d996e40bc7be
Joshua Miller-Pio <joshuapio585@gmail.com>
2023-09-25 23:06:56 +0200

    Updated file
    Updated speed, moved it up to line 1 and looped it so that it will constantly post the prompt

--- LCDwithMessage/LCDwithMessage.ino
+++ LCDwithMessage/LCDwithMessage.ino
@@ -4,23 +4,22 @@
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 void setup() {
-  
+  Serial.begin(9600);
   // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
+  lcd.begin(16, 1);
 
   // Print a message to the LCD.
-  lcd.setCursor(15, 1);
+  lcd.setCursor(15, 0);
 }
 
 void loop() {
   String word;
   lcd.autoscroll();
-  word = "Please endorse my profile               ";
-  for(static int i; i < word.length(); i++){
+  word = "Please endorse my profile   ";
+  for( int i= 0; i < word.length(); i++){
     lcd.print(word.charAt(i));
-    delay(600);
-    
+    delay(550);
   }
 
-
+   
 }
